8cd2be0e488353a1588dc605238c0e776af7993e
NemesLaszlo <wow.laszlo@gmail.com>
2022-03-17 23:06:50 +0100

    authGuard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent }
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
